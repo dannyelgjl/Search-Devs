a63a3dee1c4dd9a71aef9c071beb46353c510dc7
daniel gomes <dannyelgjl@gmail.com>
2020-01-19 22:47:45 -0300

    Calculo da geolocalizacao ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./webSocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://daniel:ad8e36@cluster0-838rc.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -10,9 +16,11 @@ mongoose.connect('mongodb+srv://daniel:ad8e36@cluster0-838rc.mongodb.net/week10?
     useCreateIndex: true,
 });
 
+// { origin: 'http://localhost:3000' }
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
